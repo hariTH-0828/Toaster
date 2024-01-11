7e535531ca12f5b27ae35e5e215b4b1ee81fb666
Hariharan <hariharan08112001@gmail.com>
2024-01-11 12:35:24 +0530

    Initial Commit

--- Toaster.podspec
+++ Toaster.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'Toaster'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of Toaster.'
+  s.summary          = 'This toaster lib is used to display success and failed toast in UIViewController'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,7 +18,7 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+  This toaster lib is used to display success and failed toast in UIViewController. Install library into project, never have to write this generic code again!
                        DESC
 
   s.homepage         = 'https://github.com/hariTH-0828/Toaster'
@@ -28,8 +28,7 @@ TODO: Add long description of the pod here.
   s.source           = { :git => 'https://github.com/hariTH-0828/Toaster.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '10.0'
-
+  s.ios.deployment_target = '12.0'
   s.source_files = 'Toaster/Classes/**/*'
   
   # s.resource_bundles = {
@@ -37,6 +36,6 @@ TODO: Add long description of the pod here.
   # }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+  s.frameworks = 'UIKit'
+  s.dependency 'SnapKit', '~>5.6.0'
 end
---
+++ Toaster/Classes/Toast.swift
@@ -0,0 +1,104 @@
+import UIKit
+import SnapKit
+
+enum actions {
+    case success
+    case failure
+}
+
+class Toast: UIStackView {
+    
+    let stateLabel: UILabel = {
+        let stateLabel = UILabel()
+        stateLabel.numberOfLines = 0
+        stateLabel.font = .boldSystemFont(ofSize: 18.0)
+        stateLabel.textAlignment = .center
+        stateLabel.textColor = .white
+        return stateLabel
+    }()
+    
+    let stateIcon: UIImageView = {
+        let icon = UIImageView()
+        icon.contentMode = .scaleAspectFit
+        icon.clipsToBounds = true
+        return icon
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.axis = .horizontal
+        self.distribution = .fillProportionally
+        self.alignment = .leading
+        self.backgroundColor = .black
+        self.layer.cornerRadius = 20
+        self.layer.opacity = 0
+    
+        self.addArrangedSubview(stateIcon)
+        stateIcon.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(5)
+            make.centerY.equalToSuperview()
+            make.width.height.equalTo(28)
+        }
+        self.addArrangedSubview(stateLabel)
+        stateLabel.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(5)
+            make.centerY.equalToSuperview()
+        }
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func show(_ viewController: UIViewController, action: actions) {
+        setupConstraint(on: viewController)
+        switch action {
+        case .success:
+            configureForSuccess()
+        case .failure:
+            configureForFailure()
+        }
+        
+    }
+    
+    private func configureForSuccess() {
+        self.layer.opacity = 1.0
+        stateLabel.text = "success"
+        stateIcon.tintColor = .systemGreen
+        stateIcon.image = UIImage(systemName: "checkmark.circle.fill")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.hide()
+        }
+    }
+    
+    private func configureForFailure() {
+        self.layer.opacity = 1.0
+        stateLabel.text = "failed"
+        stateIcon.tintColor = .systemRed
+        stateIcon.image = UIImage(systemName: "xmark.circle.fill")
+        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
+            self.hide()
+        }
+    }
+    
+    private func hide() {
+        UIView.animate(withDuration: 1.0, animations: {
+            self.layer.opacity = 0.0
+        })
+    }
+    
+    private func setupConstraint(on viewController: UIViewController) {
+        viewController.view.addSubview(self)
+        snp.makeConstraints { make in
+            make.width.equalTo(120)
+            make.height.equalTo(40)
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().inset(50)
+        }
+    }
+}
